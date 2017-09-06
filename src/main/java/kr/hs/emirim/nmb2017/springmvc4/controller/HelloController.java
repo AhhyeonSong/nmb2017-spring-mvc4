a2a68f1c24e7dd659778d79f174f7786388e8979
User <User@DESKTOP-TGA5D20>
2017-09-06 09:34:14 +0900

    /기본 컨트롤러 추가

---
+++ src/main/java/kr/hs/emirim/nmb2017/springmvc4/controller/HelloController.java
@@ -0,0 +1,12 @@
+package kr.hs.emirim.nmb2017.springmvc4.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	@RequestMapping
+	public String hello() {
+		return "Hello world!";
+	}
+}
